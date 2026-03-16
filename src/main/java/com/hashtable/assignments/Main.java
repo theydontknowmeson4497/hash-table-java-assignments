cb96d730f5d8150841e492f08cd1f9b72216c907
theydontknowmeson4497 <mm4497@srmist.edu.in>
2026-03-16 15:07:34 +0530

    Initial Java project structure

---
+++ src/main/java/com/hashtable/assignments/Main.java
@@ -0,0 +1,9 @@
+package com.hashtable.assignments;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hash Table Assignments Project Started");
+    }
+
+}
